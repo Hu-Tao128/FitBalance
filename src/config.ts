5dd1062478c0e4fbdc2eba9570b00c2b3c01da4c
Hu-Tao128 <alcantarahuerta128@gmail.com>
2025-07-29 08:55:17 -0700

    configuracion servidor nuevo

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
   //BASE_URL: 'https://fitbalance-backend.onrender.com',
-  BASE_URL: 'http://192.168.0.17:3000',
+  //BASE_URL: 'http://192.168.0.17:3000',
+  BASE_URL: 'https://api.ayasc.space',
 };
